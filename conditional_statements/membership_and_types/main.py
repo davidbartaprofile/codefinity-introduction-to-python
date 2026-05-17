fbbef177ea95e6fd0e89333d4e1b0be7f47bcab8
davidbarta <david.barta.nz@gmail.com>
2026-05-17 19:12:22 +1200

    Update main.py

--- conditional_statements/membership_and_types/main.py
+++ conditional_statements/membership_and_types/main.py
@@ -1,3 +1,12 @@
+# You are managing data for a new product that has just been added to a grocery store system. Your task is to analyze the product information using membership operators and type comparisons.
+#   Check if the substring 'raw' exists in description. Store the result in contains_raw.
+#   Check if the substring 'Imported' exists in description. Store the result in contains_Imported.
+#   Check if price is of type float. Store the result in price_is_float.
+#   Check if count is of type int. Store the result in count_is_int.
+#   Print the results exactly in the format provided in the code.
+#   Remember that python is case sensitive, so 'imported' and 'Imported' are considered different strings.
+
+
 # Product details
 description = "Imported honey, raw and unfiltered"
 price = "5.99"
