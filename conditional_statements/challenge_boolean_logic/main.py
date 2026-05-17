a723b2400425e31136ac424a6ac6c223fdc5398f
davidbarta <david.barta.nz@gmail.com>
2026-05-17 18:43:36 +1200

    Update main.py

--- conditional_statements/challenge_boolean_logic/main.py
+++ conditional_statements/challenge_boolean_logic/main.py
@@ -1,3 +1,9 @@
+#    Determine whether a grocery item should be discounted based on its seasonal status, stock level, and sales performance.
+#    Define a boolean variable overstock_risk — it should be True if the item is seasonal and its current_stock exceeds high_stock_threshold.
+#    Define a boolean variable discount_eligible — it should be True if the item is not selling_well and not on_sale.
+#    Define a boolean variable make_discount — it should be True if either overstock_risk or discount_eligible is True.
+#    Output: Print the following line, where the value of make_discount is shown at the end: "Should the item be discounted?" True
+
 seasonal = True
 on_sale = False
 selling_well = False
